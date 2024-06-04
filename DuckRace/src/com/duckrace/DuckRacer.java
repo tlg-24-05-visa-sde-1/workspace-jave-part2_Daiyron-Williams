218b3c9ab66553a4e593134624d019d3ba97a607
Daiyron Williams <DaiyronW@tlgcohort.com>
2024-06-04 13:42:22 -0500

    Iteration 1: Duck Race

---
+++ DuckRace/src/com/duckrace/DuckRacer.java
@@ -0,0 +1,51 @@
+package com.duckrace;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+class DuckRacer {
+    //  properties, fields, instance variables
+    private final int id;
+    private String name;
+    private final Collection<Reward> rewards = new ArrayList<Reward>();
+
+    //  constructors
+    public DuckRacer(int id, String name) {
+        this.id = id;
+        setName(name);
+    }
+
+    //  business or "action" methods
+    public void win(Reward reward) {
+        rewards.add(reward);
+    }
+
+    //  accessor methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String Name) {
+        this.name = Name;
+    }
+
+    public int getWins() {
+        return rewards.size();
+    }
+
+    //  NOTE: return a read-only view of the 'rewards' collection (client can't modify it!)
+    public Collection<Reward> getRewards() {
+        return Collections.unmodifiableCollection(rewards);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: id = %s, name = %s, wins = %s, rewards = %s",
+                getClass().getSimpleName(), getId(), getName(), getWins(), getRewards());
+    }
+}
